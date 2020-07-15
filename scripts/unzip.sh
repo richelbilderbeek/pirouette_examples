a297759e5a1302994924ecb79a9bd994206d8358
richelbilderbeek <richel@richelbilderbeek.nl>
2020-07-15 07:34:28 +0200

    Add unzip script

---
+++ scripts/unzip.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Unzip the zipped Peregrine runs
+#
+# Usage:
+#
+#   ./unzip.sh
+#
+unzip pirouette_example_18.zip
+unzip pirouette_example_19.zip
+unzip pirouette_example_20.zip
+unzip pirouette_example_21.zip
+unzip pirouette_example_22.zip
+unzip pirouette_example_23.zip
+unzip pirouette_example_24.zip
+unzip pirouette_example_25.zip
+unzip pirouette_example_26.zip
+unzip pirouette_example_27.zip
+unzip pirouette_example_28.zip
+unzip pirouette_example_29.zip
+unzip pirouette_example_30.zip
+unzip pirouette_example_31.zip
+unzip pirouette_example_32.zip
+unzip pirouette_example_33.zip
+unzip pirouette_example_34.zip
+unzip pirouette_example_35.zip
+unzip pirouette_example_36.zip
+unzip pirouette_example_37.zip
+unzip pirouette_example_38.zip
+unzip pirouette_example_39.zip
+unzip pirouette_example_40.zip
+unzip pirouette_example_41.zip
+unzip pirouette_example_42.zip
+unzip pirouette_example_43.zip
+unzip pirouette_example_44.zip
+unzip pirouette_example_45.zip
+unzip pirouette_example_46.zip
+unzip pirouette_example_47.zip
+unzip pirouette_example_48.zip
+unzip pirouette_example_49.zip
+
